54006a2f5fc628381af2cf066c552c9cd0eeecc3
siykook319 <2905409709@qq.com>
2026-09-06 16:55:41 +0800

    Update next.config.ts with new configuration options
    Configure output, images, base path, and asset prefix.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/nongyiba",
+  assetPrefix: "/nongyiba/",
 };
 
 export default nextConfig;
